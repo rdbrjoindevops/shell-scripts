1b07fd9c318134598135c580120a0be4c19e1d96
ravindra <rdbrjoindevops@gmail.com>
2024-12-30 20:41:48 +0530

    love letter

--- 01executme.sh
+++ 01executme.sh
@@ -2,3 +2,10 @@
 
 echo "neha hates Ravindra"
 echo "Ravindra loves neha"
+echo "My lovely wife ❤️,
+
+You are the love of my life. You are the one person who means more to me than anything else in this world. You are my best friend and my closest confidante. I love you more than words can express.❣️
+
+I am so grateful to have you by my side. You have been my rock during difficult times and my joy during the good times. You are the one person who makes me feel complete.❣️
+
+I cannot imagine my life without you. I am so lucky to be married to the most amazing woman in the world. I will love you forever. ❣️"
