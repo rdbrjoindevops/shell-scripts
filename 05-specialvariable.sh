137ab82a69536f40698ba8b17c68bffcd984edd3
ravindra <rdbrjoindevops@gmail.com>
2025-01-01 18:42:35 +0530

    push

--- 05-specialvariable.sh
+++ 05-specialvariable.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed: $@"
 echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Process id of current script: $$"
+sleep 60 &
+echo "Process id of last command in background: $!"
