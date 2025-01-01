035b2f5240ba9f895b34e9eae38dc0645f9e450f
ravindra <rdbrjoindevops@gmail.com>
2025-01-01 17:21:41 +0530

    push

--- 03datatype.sh
+++ 03datatype.sh
@@ -1,10 +1,10 @@
-#!/bin/bash/
+#!/bin/bash
 
 NUMBER1=$1
 NUMBER2=$2
-TIMESTAMP=$(date)
-echo "script executed at $TIMESTAMP"
 
+TIMESTAMP=$(date)
+echo "Script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
