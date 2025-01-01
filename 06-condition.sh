4f6c98ca805fd2dce2430ab865dd87dd39095192
ravindra <rdbrjoindevops@gmail.com>
2025-01-01 19:02:26 +0530

    push

--- 06-condition.sh
+++ 06-condition.sh
@@ -5,6 +5,7 @@ NUMBER=$1
 if [$NUMBER -lt 100]
 then
    echo "given number is less than 100"
+
  else
-     echo "given number is grater than 100
-     fi
+    echo "Given number is grater than or equal to 100"
+fi
