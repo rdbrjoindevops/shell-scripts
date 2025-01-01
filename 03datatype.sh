1015fcb9a3eb6066d6d6ee86cb65a702b01ced68
ravindra <rdbrjoindevops@gmail.com>
2025-01-01 18:50:10 +0530

    push

--- 03datatype.sh
+++ 03datatype.sh
@@ -4,8 +4,9 @@ NUMBER1=$1
 NUMBER2=$2
 
 TIMESTAMP=$(date)
-present dir=$PWD
+PWD=$(pwd)
 echo "Script executed at: $TIMESTAMP"
+echo "Present working directory: $PWD"
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
