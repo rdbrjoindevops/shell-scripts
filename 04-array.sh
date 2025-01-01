eeb9730de6a34198dd9d2f7c293c8b8ce229c7d1
ravindra <rdbrjoindevops@gmail.com>
2025-01-01 18:22:32 +0530

    push

--- 04-array.sh
+++ 04-array.sh
@@ -2,9 +2,9 @@
 
 MOVIES=("bahubal" "salar" "kalki")
 
-echo "First movie: ${MOVIES[0]}"
-echo "second movie: ${MOVIES[1]}"
-echo "Third movie: ${MOVIES[2]}"
+echo "First movie: {MOVIES[0]}"
+echo "second movie: {MOVIES[1]}"
+echo "Third movie: {MOVIES[2]}"
 
-echo "All movies are: ${MOVIES[@]}"
+echo "All movies are: {MOVIES[@]}"
 
