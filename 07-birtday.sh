bf06eb729cc9c2e61c72914d71f13b3294f0b1d4
ravindra <rdbrjoindevops@gmail.com>
2025-01-01 19:20:43 +0530

    push

--- 07-birtday.sh
+++ 07-birtday.sh
@@ -8,7 +8,7 @@ my_birthday="01-01"
 
 # Check if today is your birthday
 if [[ "$today" == "$my_birthday" ]]; then
-  echo "Happy Birthday to you Mihira!"
+  echo "Happy Birthday to you Mihira! Happy Birthday to the most amazing girl I know! You're smart, kind, and so much fun to be around. May your day be filled with laughter, joy, and all your favorite things. You deserve the world, and I hope you have a birthday that's as special as you are"
 else
   echo "Today is not your birthday."
 fi
