c5d147abf6dcdbacdae3b646552f32e9e75874d3
ravindra <rdbrjoindevops@gmail.com>
2025-01-01 18:36:19 +0530

    push

--- 04-array.sh
+++ 04-array.sh
@@ -7,7 +7,7 @@ echo "First movie: ${MOVIES[2]}"
 echo "second movie: ${MOVIES[1]}"
 echo "Third movie: ${MOVIES[0]}"
 
-echo "movie COLLECTIONS: ${COLLECTION]}"
+echo "movie COLLECTIONS: ${COLLECTION}"
 
 
 echo "All movies are: ${MOVIES[@]}"
