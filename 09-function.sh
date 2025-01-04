b386bc1daa651ec7f820aa441fa6f571ba740f88
ravindra <rdbrjoindevops@gmail.com>
2025-01-04 11:43:40 +0530

    push

---
+++ 09-function.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDETE(){
+    if [ $1 -ne 0 ]
+    then 
+  echo "$2 ...failure"
+  exit 1
+  else 
+  echo "$2 ...success"
+
+}
+
+if [ $USERID -ne 0 ]
+then
+
+echo " Error:: you must have sudo access to execute this script"
+exit 1
+
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+
+dnf install mysql -y
+VALIDTE $? "INSTALL MYSQL"
+
+else 
+echo " MySQL already installed"
+
+
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+
+dnf install nginx -y
+VALIDTE $? "INSTALL nginx"
+
+else 
+echo " nginx already installed"
+
+
+fi
+
+
